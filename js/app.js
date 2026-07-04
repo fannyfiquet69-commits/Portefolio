eb7e92c8a35f9e8a7740364ecce5733647523414
fannyfiquet69-commits <fannyfiquet69@gmail.com>
2026-07-04 16:28:38 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -125,8 +125,8 @@ const droneGalleries = {
     ],
 
     videos: [
-        "video/drone-1.mp4",
-        "video/drone-2.mp4"
+        "video/drone-1.MP4",
+        "video/drone-2.MP4"
     ]
 
 };
